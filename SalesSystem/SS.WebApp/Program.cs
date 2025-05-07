a50cac671dc08879b7b7f7790a435c886d23c05c
Joaquin Tealdi <joaquintealdi25@gmail.com>
2025-05-07 17:03:57 -0300

    add Nlog package

--- SalesSystem/SS.WebApp/Program.cs
+++ SalesSystem/SS.WebApp/Program.cs
@@ -1,11 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 using SS.DAL;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Nlog configuration
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
+
+
+var logPath = Path.Combine(Directory.GetCurrentDirectory(), "logs");
+if (!Directory.Exists(logPath))
+{
+    Directory.CreateDirectory(logPath);
+}
 
 //ConnectionString
 builder.Services.AddDbContext<SSDbContext>(options =>
@@ -13,7 +26,11 @@ builder.Services.AddDbContext<SSDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+builder.Services.AddScoped<testt>();
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
